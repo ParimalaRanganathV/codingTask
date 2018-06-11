49e0ea0fec27bfc5b823a2d9595fdb5dc756aebc
Parimala Ranganath Velayudam <parimalaranganathvelayudam@parimalas-macbook-air-2.local>
2018-06-11 23:42:05 +0530

    initial configuration is done

---
+++ codingTask/LocationDataModel.swift
@@ -0,0 +1,18 @@
+//
+//  LocationDataModel.swift
+//  codingTask
+//
+//  Created by Parimala Ranganath Velayudam on 11/06/18.
+//  Copyright © 2018 VPR productions. All rights reserved.
+//
+
+import Foundation
+
+
+class LocationDataModel  {
+    var title: String = ""
+    var snippet: String = ""
+    var latitude: Double = 0.0
+    var longitude: Double = 0.0
+    
+}
